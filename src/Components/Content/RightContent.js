b24ec309a0b8dbf44e33f6d45f90a9627716808c
KimHan2074 <ngothikimhan.2004@gmail.com>
2025-04-22 08:04:31 +0700

    done create and fetch api

--- src/Components/Content/RightContent.js
+++ src/Components/Content/RightContent.js
@@ -10,7 +10,7 @@ class RightContent extends Component {
     }
 
     componentDidMount() {
-        fetch('https://656ca88ee1e03bfd572e9c16.mockapi.io/products') 
+        fetch('https://api-products-2-ottk.onrender.com/api/products')
             .then(response => response.json())
             .then(data => {
                 const filteredData = data.map(item => ({
